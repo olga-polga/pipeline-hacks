8f82cea89d0233b181f4b57d0a38bc597953fe25
Olga <olga.penina@emc.com>
2017-08-28 21:01:00 -0400

    loaf from git

---
+++ hello.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+clear
+
+
+echo -e "\e[1mbold\e[0m"
+echo -e "\e[3mitalic\e[0m"
+echo -e "\e[4munderline\e[0m"
+echo -e "\e[9mstrikethrough\e[0m"
+echo -e "\e[36mHello World\e[0m"
+echo -e "\x1B[32mHello World\e[0m"
+echo -e "\x1B[35mHello World\e[0m  the end"
